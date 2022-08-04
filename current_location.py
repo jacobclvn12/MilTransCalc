8856db4a6ae219c88cf72a0cd1acc2d3944c41d0
Jacob M. Colvin <jacobclvn12@gmail.com>
2022-08-04 12:40:24 -0400

    added Cost of Living lookup using CSV file

--- current_location.py
+++ current_location.py
@@ -1,9 +1,18 @@
-
-
+import csv
+path = r'/home/jmcolvin/Desktop/MilTransCalc/cost_living_index.csv'
 
 class Current_location:
     
     def __init__(self, city, state):
         self.city = city
         self.state = state
-        self.current_cli = None      #look up cost of living index from city state on CSV file
+        self.current_cli = 0
+        with open(path) as cost_living_index:
+            reader = csv.DictReader(cost_living_index)
+            for line in reader:
+                if self.city == line['City'] and self.state == line['State']:
+                    self.current_cli = int(line['Index'])
+
+current_city = Current_location('Baltimore', 'MD')
+
+print(current_city.current_cli)
--- new_location.py
+++ new_location.py
@@ -1,7 +1,5 @@
-
-
-from sre_parse import State
-from types import CoroutineType
+import csv
+path = r'/home/jmcolvin/Desktop/MilTransCalc/cost_living_index.csv'
 
 
 class New_Location:
@@ -9,5 +7,10 @@ class New_Location:
     def __init__(self, city, state):
         self.city = city
         self.state = state
-        self.new_cli = None     #find Cost of lIving index in CSV
+        self.new_cli = 0
         self.state_tax = None   #find in CSV
+        with open(path) as cost_living_index:
+            reader = csv.DictReader(cost_living_index)
+            for line in reader:
+                if self.city == line['City'] and self.state == line['State']:
+                    self.new_cli = int(line['Index'])
